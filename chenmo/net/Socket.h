357b8c241ccd69e27757b290b53e58fcceee50fa
tfhnia <tfhnia@gmail.com>
2020-10-22 09:18:29 +0800

    封装socket;新增Acceptor

---
+++ chenmo/net/Socket.h
@@ -0,0 +1,54 @@
+#ifndef CHENMO_NET_SOCKET_H
+#define CHENMO_NET_SOCKET_H
+
+#include "chenmo/base/noncopyable.h"
+
+struct tcp_info;
+
+namespace chenmo
+{
+
+namespace net
+{
+
+class InetAddress;
+
+class Socket : noncopyable
+{
+public:
+    explicit Socket(int sockfd)
+      : sockfd_(sockfd)
+    {}
+
+    ~Socket();
+
+    int fd() const { return sockfd_; }
+    bool getTcpInfo(struct tcp_info*) const;
+    bool getTcpInfoString(char* buf, int len) const;
+
+    void bindAddress(const InetAddress& localaddr);
+
+    void listen();
+
+    int accept(InetAddress* peeraddr);
+
+    void shutdownWrite();
+
+    void setTcpNoDelay(bool on);
+
+    void setReuseAddr(bool on);
+
+    void setReusePort(bool on);
+
+    void setKeepAlive(bool on);
+private:
+    const int sockfd_;
+
+};
+
+
+}
+
+}
+
+#endif
